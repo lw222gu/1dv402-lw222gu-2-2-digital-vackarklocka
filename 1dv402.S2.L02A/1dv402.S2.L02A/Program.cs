d4e09400ce90ee3732b7893be15d0a114404a049
Lisa Westlund <lw222gu@student.lnu.se>
2014-10-12 15:19:20 +0200

    En första commit

---
+++ 1dv402.S2.L02A/1dv402.S2.L02A/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1dv402.S2.L02A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
